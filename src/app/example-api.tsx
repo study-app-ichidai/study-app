439af44987c6a939cf71dcbebdb4e2c37f8821d6
PoPodada <goito.722@icloud.com>
2025-03-14 22:16:22 +0900

    簡単なディレクトリ構成を追加

--- src/app/example-api.tsx
+++
@@ -1,9 +0,0 @@
-'use server'
-import { createClient } from '@/utils/supabase/server'
-
-export const ExampleApi = async () => {
-  const supabase = await createClient()
-  const { data: instruments } = await supabase.from('instruments').select()
-
-  return <div>{JSON.stringify(instruments)}</div>
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { ExampleApi } from '@/app/example-api'
-import { Button } from '@/components/ui/button'
-
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Button>test</Button>
-      </main>
-      <ExampleApi />
-    </div>
-  )
+export default function RootPage() {
+  return <>root page</>
 }
